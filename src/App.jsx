e2ca5804c3adaf85ee27b434a555065658c71ab1
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-08 16:15:42 +0200

    Refactor header

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,31 @@ import BgMobileLight from '../public/images/bg-mobile-light.jpg'
 import Logo from '../public/images/logo.png'
 
 import ThemeMode from './components/ThemeMode'
+import AddTodoInput from './components/AddTodoInput'
 
 function App() {
-  const [isThemeMode, setIsThemeMode] = useState(false);
+  const [isThemeMode, setIsThemeMode] = useState(true);
 
   const handleThemeMode = () => {
     setIsThemeMode(!isThemeMode);
   }
 
   return (
-    <main className={`h-screen flex flex-col gap-10 ${isThemeMode ? 'light-mode' : 'dark-mode'}`}>
-      <header className='relative z-10'>
-        <div className='absolute w-full flex flex-row justify-between items-center px-6 pt-12'>
-          <img className='w-[108px]' src={Logo} alt="Logo TODO" />
-          <ThemeMode onThemeMode={handleThemeMode} isThemeMode={isThemeMode}/>
-        </div>
-        <picture className='absolute -z-10'>
-          <img src={BgMobileLight} alt="Background Mobile Light" />
-        </picture>
-      </header>
+    <main className={`h-screen relative flex flex-col ${isThemeMode ? 'light-mode' : 'dark-mode'}`}>
+      <picture className='absolute z-10'>
+        <img src={BgMobileLight} alt="Background Mobile Light" />
+      </picture>
+      <section className='h-screen flex flex-col gap-10 z-10'>
+        <header>
+          <div className='w-full flex flex-row justify-between items-center px-6 pt-12'>
+            <img className='w-[108px]' src={Logo} alt="Logo TODO" />
+            <ThemeMode onThemeMode={handleThemeMode} isThemeMode={isThemeMode}/>
+          </div>
+        </header>
+        <section className='px-6'>
+          <AddTodoInput />
+        </section>
+      </section>
     </main>
   )
 }
---
+++ src/components/AddTodoInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddTodoInput() {
+  return (
+    <div className='px-6 bg-white preset-3'>
+        <input type="text" placeholder='Create a new todo...' />
+    </div>
+  )
+}
