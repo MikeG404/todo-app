96fbb07e994072137cf6a41690538ee57797d0dd
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-08 15:45:09 +0200

    Positionning Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import './App.css'
+import { useState } from 'react'
 
 import BgMobileLight from '../public/images/bg-mobile-light.jpg'
 // import BgMobileDark from '../public/images/bg-mobile-dark.jpg'
 import ThemeMode from './components/ThemeMode'
 
 function App() {
+  const [isThemeMode, setIsThemeMode] = useState(false);
+
+  const handleThemeMode = () => {
+    setIsThemeMode(!isThemeMode);
+  }
 
   return (
     <main>
-      <picture>
-        <img src={BgMobileLight} alt="Background Mobile Light" />
-      </picture>
-      <header className='flex flex-row items-center px-6'>
-        <h1>TODO</h1>
-        <ThemeMode />
+      <header className='relative z-10'>
+        <div className='absolute w-full flex flex-row justify-between items-center px-6 pt-12'>
+          <h1>TODO</h1>
+          <ThemeMode onThemeMode={handleThemeMode} isThemeMode={isThemeMode}/>
+        </div>
+        <picture className='absolute -z-10'>
+          <img src={BgMobileLight} alt="Background Mobile Light" />
+        </picture>
       </header>
     </main>
   )
--- src/components/ThemeMode.jsx
+++ src/components/ThemeMode.jsx
@@ -1,11 +1,14 @@
 import IconMoon from '../../public/images/icon-moon.svg'
 import IconSun from '../../public/images/icon-sun.svg'
 
-export default function ThemeMode({ onClick }) {
+export default function ThemeMode({ onThemeMode, isThemeMode }) {
   return (
-    <div>
-        <img src={IconMoon} alt="Icon Moon" />
-        <img src={IconSun} alt="Icon Moon" />
-    </div>
+    <button onClick={onThemeMode} className='cursor-pointer'>
+        { isThemeMode ? 
+            <img src={IconMoon} alt="Icon Moon" style={{color: 'black'}}/>
+        :
+            <img src={IconSun} alt="Icon Moon" />
+        }
+    </button>
   )
 }
