a204befda304015cbef39d357dbbf8ff5604fdfd
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-19 06:19:51 +0200

    Add delete, read and create request

---
+++ services/todo.service.js
@@ -0,0 +1,52 @@
+const url = ' http://localhost:3000/todo/'
+
+export const todoService = {
+    getTodos: async () => {
+        try {
+            const response = await fetch(url);
+            
+            if (!response.ok) {
+                throw new Error("Network Error " + response.status);
+            }
+    
+            return response.json();
+        } catch (error) {
+            throw new Error(error.message);
+        }
+
+    },
+
+    addTodo: async (title) => {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(title)
+            })
+
+            if (!response.ok) {
+                throw new Error("Creation resuest failed " + response.status);
+            }
+
+            return response.json();
+        } catch (error) {
+            console.log(error.message);
+        }
+    },
+
+    deleteTodo: async (id) => {
+        try {
+            const response = await fetch(`${url}/${id}`, {
+                method: 'DELETE',
+            })
+
+            if (!response.ok) {
+                throw new Error("Creation resuest failed " + response.status);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+} 
